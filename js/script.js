c3db8abbf81ce3ad133de056c66a62cbde5c9964
lirenneedhelp <kohliren0707@gmail.com>
2024-10-05 22:16:20 +0800

    migrating new website

--- js/script.js
+++ js/script.js
@@ -1,111 +1,69 @@
-let counter1 = 0;
-let counter2 = 1;
-
-const sections = document.querySelectorAll("section");
-const progress = document.querySelector(".progress h2");
-const circles = document.querySelectorAll(".circle");
-
-for (var i = 0; i < sections.length; ++i)
-{
-  if (sections[i].className != "section-1")
-  {
-    sections[i].style.opacity = 0.0;
-    sections[i].style.zIndex = "10";
-  }
-  else
-  {
-    sections[i].style.zIndex = "20";
-  }
+function downloadFile() {
+    const link = document.createElement('a');
+    link.href = 'files/resume.pdf';  // Replace with the path to your CV
+    link.download = 'liren_cv.pdf';   // The name the file will be downloaded as
+    link.click();
 }
-// Circle Progress Logic
-const progressCounter = () => {
-  progress.textContent = `${counter2}/${sections.length}`;
 
-  Array.from(circles).forEach((circle) => {
-    circle.style.backgroundColor = "transparent";
-  });
-  document.querySelector(`.circle-${counter2}`).style.backgroundColor = "#ddd";
-};
-function pauseVideo()
+function hamburg()
 {
-  document.getElementById("video"+counter2).onpause();
-}
-// Controls which page appears via Scaling // CHANGED
-
-const pageController = () => {
-  // If it reaches the last page, then return back to the first page
-  if (counter1 === 7) {
-    counter1 = 0;
-    counter2 = 1;
-    progressCounter();
-  }
-
-  if (counter1 === -1) { // If you scroll backwards from the first page, it will bring you back to the last page
-    counter1 = 6;
-    counter2 = 7;
-    progressCounter();
-  }
-  progressCounter();
-};
-
-
-// Left Button Logic
-document.querySelector(".left-btn").addEventListener("click", () => {
-  //pauseVideo();
-  console.log(counter1, counter2);
-  counter1--;
-  counter2--;
-  pageController();
-    for (var i = 0; i < sections.length; ++i)
-    {
-      if (sections[i].className != "section-"+counter2)
-      {
-        sections[i].style.opacity = 0.0;
-        sections[i].style.zIndex = "10";
-
-      }
-      else
-      {
-        sections[i].style.transitionProperty = "opacity";
-        sections[i].style.transitionDuration = "1s";
-        sections[i].style.opacity = 1.0;
-        sections[i].style.zIndex = "20";
-      }
+    const hamburg = document.querySelector('.hamburg')
+    const cancel = document.querySelector('.cancel')
+    const navbar = document.querySelector('.dropdown');
     
+    hamburg.addEventListener('click', ()=>{
+        navbar.style.transform = "translateY(0px)"
+        hamburg.style.display = 'none'
+    })
+    cancel.addEventListener('click', ()=>{
+        navbar.style.transform = "translateY(-300px)"
+        hamburg.style.display = 'block'
+    })
+}
 
-    /*document.querySelector(`.section-${counter2}-wrapper`).style.opacity =
-      "0.0";
-    document.querySelector(`.section-${counter2 + 1}-wrapper`).style.opacity =
-      "1.0";*/
-  }
-});
+function cancel()
+{
 
-// Right Button Logic
-document.querySelector(".right-btn").addEventListener("click", () => {
-  //pauseVideo();
-  counter1++;
-  counter2++;
+}
 
-  pageController();
-  console.log(counter1, counter2);
 
-  for (var i = 0; i < sections.length; ++i)
-  {
-    if (sections[i].className != "section-"+counter2)
-    {
-      sections[i].style.opacity = 0.0;
-      sections[i].style.zIndex = "10";
+const texts = [
+    "GAME DEVELOPER",
+    "AI ENGINEER",
+    "SOFTWARE ENGINEER"
+]
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let charIndex = 0;
+
+function typeWriter(){
+    if (charIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charIndex)
+        charIndex++;
+        setTimeout(typeWriter, speed);
     }
     else
     {
-      sections[i].style.transitionProperty = "opacity";
-      sections[i].style.transitionDuration = "1s";
-      sections[i].style.opacity = 1.0;
-      sections[i].style.zIndex = "20";
+        setTimeout(eraseText, 1000);
     }
-  /*document.querySelector(`.section-${counter2}-wrapper`).style.opacity =
-    "1.0";
-  document.querySelector(`.section-${counter1}-wrapper`).style.transform =
-    "0.0";*/
-  
-}});
+}
+
+
+function eraseText()
+{
+    if (textElements.innerHTML.length > 0)
+    {
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText,50);
+    }
+    else
+    {
+        textIndex = (textIndex + 1) % texts.length
+        charIndex = 0
+        setTimeout(typeWriter,500);
+    }
+}
+window.onload = typeWriter
